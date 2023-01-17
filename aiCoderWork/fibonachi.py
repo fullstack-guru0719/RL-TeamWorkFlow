62835207845e7230a08c92527d37c74b4e6fd9b9
richard <satellite19900502@gmail.com>
2023-01-17 11:27:39 -0500

    #aiCoderWork:addSecondFile

---
+++ aiCoderWork/fibonachi.py
@@ -0,0 +1,12 @@
+a = input("This is a num1: ")
+b = input('This is a num2: ')
+
+sum = int(a) + int(b)
+sub = int(a) - int(b)
+mul = int(a) * int(b)
+div = float(a) / float(b)
+
+print('sum of {0} and {1} is {2}'.format(a,b,sum))
+print('sub of {0} and {1} is {2}'.format(a,b,sub))
+print('mul of {0} and {1} is {2}'.format(a,b,mul))
+print('div of {0} and {1} is {2}'.format(a,b,div))
