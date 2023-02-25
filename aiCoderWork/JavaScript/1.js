14a0cbd276b75eb53aea0cb7243a9c0ac63ddb29
gillhousekevin <gillhousekevin@gmail.com>
2023-02-24 19:01:41 -0500

    add 2023-2-25

--- aiCoderWork/JavaScript/1.js
+++ aiCoderWork/JavaScript/1.js
@@ -1,5 +1,10 @@
-function test(a, b) {
-  return a * b;
+function myDisplayer(something) {
+  document.getElementById('demo').innerHTML = something;
 }
 
-test(2, 5);
+function myCalculator(num1, num2, myCallback) {
+  let sum = num1 + num2;
+  myCallback(sum);
+}
+
+myCalculator(5, 5, myDisplayer);
